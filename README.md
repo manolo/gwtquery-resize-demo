632389c3ac27b31f26d64fe784f3f029368db3bc
Manolo Carrasco <manolo@apache.org>
2015-03-20 00:17:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+Examples for the gQuery Resize plugin.
+
+[demo](http://manolo.github.io/gwtquery-resize-demo/index.html)
+
+@Author Manuel Carrasco Moñino
+
---
+++ src/main/java/com/google/gwt/query/client/plugins/ResizeSample.java
@@ -0,0 +1,28 @@
+package com.google.gwt.query.client.plugins;
+
+import static com.google.gwt.query.client.GQuery.$;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.query.client.Function;
+import com.google.gwt.query.client.GQuery;
+
+/**
+ */
+public class ResizeSample implements EntryPoint {
+
+  public void onModuleLoad() {
+    $(".container").as(Resize.Resize).resize(new Function() {
+      public void f() {
+        GQuery t = $(this).find(".test");
+        $(this).find(".info").text(t.width() + "x" + t.height());
+      }
+    }).trigger("resize");
+
+    $(".add_text").click(new Function(){
+      public void f() {
+        $(this).parent().append(" Adding some more text, to grow the parent container!");
+      }
+    });
+  }
+
+ }
