c6464aa27c8b604d8b07eab83f80cc654bc57e42
Manolo Carrasco <manolo@apache.org>
2015-03-20 19:42:09 +0100

    Add window resize example. Improve wording

--- src/main/java/com/google/gwt/query/client/plugins/ResizeSample.java
+++ src/main/java/com/google/gwt/query/client/plugins/ResizeSample.java
@@ -1,6 +1,7 @@
 package com.google.gwt.query.client.plugins;
 
 import static com.google.gwt.query.client.GQuery.$;
+import static com.google.gwt.query.client.GQuery.window;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.query.client.Function;
@@ -11,6 +12,7 @@ import com.google.gwt.query.client.GQuery;
 public class ResizeSample implements EntryPoint {
 
   public void onModuleLoad() {
+
     $(".container").as(Resize.Resize).resize(new Function() {
       public void f() {
         GQuery t = $(this).find(".test");
@@ -18,11 +20,18 @@ public class ResizeSample implements EntryPoint {
       }
     }).trigger("resize");
 
-    $(".add_text").click(new Function(){
+    $(".add_text").click(new Function() {
       public void f() {
         $(this).parent().append(" Adding some more text, to grow the parent container!");
       }
     });
+
+    $(window).as(Resize.Resize).resize(new Function(){
+      public void f() {
+        GQuery w = $(window);
+        $(".info").eq(0).text(w.width() + "x" + w.height());
+      }
+    }).trigger("resize");
   }
 
  }
